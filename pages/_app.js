d5c3dde9440e860fe6952c0d1002408a2737ffab
monoj kumar <monojkumar57859893@gmail.com>
2024-02-11 02:52:14 +0600

    create google anlayes code

--- pages/_app.js
+++ pages/_app.js
@@ -7,16 +7,35 @@ import "../styles/css/style.css";
 import "../styles/css/custom.css";
 
 import { Fragment, useEffect } from "react";
-import { useRouter } from "next/router";
-import TagManager from "react-gtm-module";
+
 function MyApp({ Component, pageProps }) {
   const Layout = Component.layout || Fragment;
 
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+   
+
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+    
+          <script
+           strategy="lazyOnload"
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-SF8D61T2PB"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SF8D61T2PB');
+            `,
+            }}
+          />
+     
+
     </>
   );
 }
